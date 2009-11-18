1783da8a1917a363bc679de7a1676d3a6b0fd1d7
phil <phil@pchowtos.co.uk>
2009-11-18 16:13:59 +0000

    Changed version number to make it GS specific

--- src/config.h
+++ src/config.h
@@ -128,7 +128,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "1.0"
+#define VERSION "1.0-gs"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
